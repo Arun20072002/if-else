7227be939cce094bdabeb9add31c671500e4b067
Arunkum20072002 <arunkumarmarikani10@gmail.com>
2023-11-06 10:02:34 +0530

    if-else

---
+++ if.js
@@ -0,0 +1,130 @@
+// Check the number is positive or negative
+var a=0;
+if(a>0){
+    console.log("this is positive number");
+}
+else if(a<0){
+    console.log("This is negative number");
+}
+else{
+    console.log("This is Zero");
+}
+// Check the number is Even or Odd
+var a=20
+if(a/2%0){
+    console.log("This is even number")
+}
+else{
+    console.log("This is odd number");
+}
+// Check the number is Two digit number
+var a=9;
+if(a>9){
+    console.log("This number is Two digit number");
+}
+else{
+    console.log("This number is one digit number");
+}
+// Check the person is eligible for vote
+var vage=17;
+if(vage>=18){
+    console.log("Your are eligible for vote")
+}
+else if(vage<18){
+    console.log("Your are not eligible for vote")
+}
+// Check the person is eligible for license
+var lage=18;
+if(lage>=18){
+    console.log("Your eligible for a licence")
+}
+else if(lage<18){
+    console.log("Your do not eligible for a licence")
+}
+// Check any two numbers are equal or grater than or less than
+var a=19,b=19;
+if(a==b){
+    console.log("Two number are equal")
+}
+else if(a!==b){
+    console.log("Two number are not equal");
+}
+// Check password is correct or not
+var regpass="a6un";
+var leginpass="a6u";
+if(regpass===leginpass){
+    console.log("your password correct welcome");
+}
+else{
+    console.log("Enter the correct password");
+}
+// Check password length(8) is strong or weak..
+var regpass="9";
+if(regpass>=8){
+    console.log("Your password is strong");
+}
+else{
+    console.log("Your password is weak");
+}
+// Student Grade 
+var sm=39;
+if(sm>=90){
+    console.log("Your grade A")
+}
+else if(sm>=80){
+    console.log("Your grade B")
+}
+else if(sm>=60){
+    console.log("Your grade C")
+}
+else if(sm>=40){
+    console.log("Your grade D")
+}
+else if(sm<=40){
+    console.log("Your fail")
+}
+// Saving Amount:
+var sa=60000;
+if(sa<=2000){
+    console.log("You can buy only basic mobile");
+}
+else if(sa<=20000){
+      console.log("You can buy only Android Mobile");  
+}
+else if(sa<=50000){
+    console.log("You can buy only IPAD");  
+}
+else if(sa>50000){
+    console.log("You can buy only Apple"); 
+}
+// Marriage eligibility:
+var age=11;
+var gender="female";
+
+if(gender=="male"){
+    if(age>=21){
+        console.log("Male-----Your are eligible for marriage" )
+    }
+    else{
+        console.log("Male-----Your are eligible for marriage")
+    }
+}
+else if(gender=="female"){
+    if(age>=18){
+        console.log("female-----Your are eligible for marriage" )
+    }
+    else{
+        console.log("female-----Your not are eligible for marriage")
+    }
+}
+// Check the given number is divisible by 6
+var a=5;
+if(a/6){
+    console.log("A is divisiable")
+}
+else{
+    console.log("A is not divisiable")
+}
+
+
+
